eb40a968cf1d403f1e390de5b7ebf2437cf17f1c
Israel <israelblenn@gmail.com>
2024-12-20 15:57:12 +1100

    adding graphql limit

--- config/plugins.js
+++ config/plugins.js
@@ -1,12 +1,17 @@
 module.exports = ({ env }) => ({
-    upload: {
-      config: {
-        providerOptions: {
-          local: {
-            path: '/data/uploads',
-          },
+  upload: {
+    config: {
+      providerOptions: {
+        local: {
+          path: '/data/uploads',
         },
       },
     },
-  });
-  
+  },
+  graphql: {
+    config: {
+      defaultLimit: 100, // Default number of items returned per query
+      maxLimit: 1000,    // Maximum number of items allowed per query
+    },
+  },
+});
